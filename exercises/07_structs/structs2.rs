18d1b002fc1d6e91775e8226163f0b499a06914a
Jeffery Samatthiya <142444167+ihartocompysci@users.noreply.github.com>
2026-01-28 13:24:51 -0800

    ..order_template does
    if the owner changes, it MOVES - "Bob" becomes "Hacker in Rust" and "Bob" is now recinded into the abyss
    if the owner stays the same, the template copies over the value

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -35,6 +35,16 @@ mod tests {
 
         // TODO: Create your own order using the update syntax and template above!
         // let your_order =
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            year: order_template.year,
+            made_by_phone: order_template.made_by_phone,
+            made_by_mobile: order_template.made_by_mobile,
+            made_by_email: order_template.made_by_email,
+            item_number: order_template.item_number,
+            count: 1,
+            ..order_template
+        };
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
